65cfb26fb44f6cfe18cb6e25b6181a3630dbe7c2
Charles2k <ejeabochachikodili@gmail.com>
2023-01-08 23:17:38 +0000

    correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,34 @@
 #include "lists.h"
 
 /**
-  * is_palindrome - checks if a singly linked list is a palindrome
-  * @head: double pointer to list.
-  *
-  *Return: 1 if palindrome, 0 if not palindrom.
-  */
+ * checkPalindrome - recursive function for checking palindrome
+ * @left: node
+ * @right: node
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int checkPalindrome(listint_t **left, listint_t *right)
+{
+	int res = 0;
+
+	if (right == NULL)
+		return (1);
+
+	res = checkPalindrome(left, right->next) &&
+		((*left)->n == right->n);
+	(*left) = (*left)->next;
+
+	return (res);
+}
+/**
+ * is_palindrome - call the above function
+ * @head: head of a list
+ * Return: 1 if palindome, 0 otherwise
+ */
+
 int is_palindrome(listint_t **head)
 {
-	return (checkPalindrome(head, *head));
-}
-
-/**
-  * checkPalindrome - recursive function ot check if sinly linked list
-  * is a palindrome.
-  * @headptr: double pointer to list.
-  * @tptr: pointer to list.
-  *
-  * Return: 1 or 0
-  */
-int checkPalindrome(listint_t **headptr, listint_t *tptr)
-{
-	int res;
-
-	/* base case */
-	if (tptr == NULL)
+	if (*head == NULL)
 		return (1);
-	res = checkPalindrome(headptr, tptr->next) && ((*headptr)->n == tptr->n);
-	return (res);
+	return (checkPalindrome(&(*head), *head));
 }
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
+    for i in matrix:
+        for j in range(0, len(i)):
+            print("{:d}".format(i[j]), end="")
+            if j != len(i) - 1:
+                print(" ", end="")
         print("")
