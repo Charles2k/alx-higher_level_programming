ef930858b6a1eca11368e8ad230778aa15bbc162
Charles2k <ejeabochachikodili@gmail.com>
2023-04-13 04:45:31 +0000

    done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,10 @@
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+	if (newDict[dict[key]] === undefined) {
+	newDict[dict[key]] = [key];
+	} else {
+	newDict[dict[key]].push(key);
   }
 }
 console.log(newDict);
